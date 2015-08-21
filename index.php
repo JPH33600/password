8714e5f30d7bd1043beab55b42805c3508cfbcf4
jerome <blachasivil@sfr.fr>
2015-08-21 19:52:21 +0200

    bon avancement de la partie front

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>formulaire d'identification</title>
+		<meta charset="utf-8"> 
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="Formulaire de d'authentification par JPH33600">
+
+	    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+	    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	    <link href="password.css" rel="stylesheet"><meta>
+	</head>
+
+	<body>
+		<div class="container col-xs-12">
+			<form>
+				<h1>AUTHENTIFICATION</h1>
+				<div class="row">
+					<input type="text" required name="name" id="name" autofocus placeholder="NOM" class="col-xs-10 col-xs-offset-1">
+					<input type="text" required name="password" id="password" placeholder="mot de passe" class="row">
+					<input type="submit" class="btn-custom col-xs-2 col-xs-offset-5" value="OK">
+				</div>
+				
+			</form>
+		</div>	
+	</body>
+
+</html>
